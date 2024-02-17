880fe82511af7333557ecac6187b72650e456dbe
apexa199 <apexaajudiya9@gmail.com>
2024-02-17 13:22:10 +0530

    p

--- Ecommerce_App_Saga/src/redux/store/store.js
+++
@@ -1,9 +0,0 @@
-import { applyMiddleware, compose, createStore} from "redux";
-import reducer from "../reducers/rootReducer";
-import {thunk} from "redux-thunk";
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
-
-const store = createStore(reducer,composeEnhancers(applyMiddleware(thunk)));
-
-export default store;
